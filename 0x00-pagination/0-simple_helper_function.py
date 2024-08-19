d8e5c00481bef07bb13a5cfc142ebdb642b20103
Cephas-Muhia <cephaschronixx@gmail.com>
2024-08-19 10:50:33 +0300

    Simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -10,4 +10,9 @@ def index_range(page, page_size):
     '''
     start = (page - 1) * page_size
     end = page * page_size
+    '''
+    In Python, when you slice a list using list[start:end], it includes the element at start but ex    cludes the element at end. So, list[10:20] will include the elements from index 10 to 19.
+    '''
     return start, end
+    
+
