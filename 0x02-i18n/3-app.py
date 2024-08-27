10be9ccb330fc0cb01350f3f286e6f11de7a6cb4
Cephas-Muhia <cephaschronixx@gmail.com>
2024-08-28 00:43:21 +0300

    Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -3,7 +3,7 @@
 """
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
@@ -21,14 +21,25 @@ class Config(object):
 # configure the flask app
 app = Flask(__name__)
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
 babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """_summary_
+
+    Returns:
+            _type_: _description_
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index():
     """_summary_
     """
-    return render_template('1-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
